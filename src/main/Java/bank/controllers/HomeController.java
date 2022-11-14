c9d30b1caf9a42eebbe09fec157ee035d2e43a51
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-11-13 21:07:06 -0600

    Established view to localhost

--- src/main/Java/bank/controllers/HomeController.java
+++ src/main/Java/bank/controllers/HomeController.java
@@ -1,4 +1,19 @@
 package bank.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    //Needs index()
+    //      login()
+    //      logout()
+    //      register()
+    //
+
+    @GetMapping("")
+    public String index() {
+        return "index";
+    }
 }
