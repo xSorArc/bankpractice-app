25c2726d1a3a88d37a99b439d85b28f2319fb814
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-11-13 18:19:24 -0600

    Added Abstract class & template files

---
+++ src/main/Java/bank/models/AbstractEntity.java
@@ -0,0 +1,29 @@
+package bank.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
