99ea8166b28477755c56374ad7cd4c95092352ac
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-11-25 22:57:49 -0600

    Created controller for user, added TODOs

--- src/main/Java/bank/controllers/UserController.java
+++ src/main/Java/bank/controllers/UserController.java
@@ -1,2 +1,33 @@
-package bank.controllers;public class UserController {
+package bank.controllers;
+
+import bank.data.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    UserRepository userRepository;
+
+    @GetMapping("profile")
+    public String displayProfile(Model model) {
+        return "/user/profile";
+    }
+
+    //TODO: Add delete user handlers
+    @GetMapping("delete")
+    public String deleteUser(Model model) {
+
+        return "";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteUser(Model model, Integer userId) {
+
+        return "";
+    }
 }
